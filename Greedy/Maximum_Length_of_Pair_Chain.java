87cac9382f0890b6d5240f03c1ea4a42f3c6794d
Vaibhav Goyal <122206348+vaibhavh200@users.noreply.github.com>
2025-07-01 09:21:16 +0530

    Update

--- Greedy/Maximum_Length_of_Pair_Chain.java
+++ Greedy/Maximum_Length_of_Pair_Chain.java
@@ -1,1 +1,18 @@
+class Solution {
+    public int findLongestChain(int[][] pairs) {
+        Arrays.sort(pairs,(a,b)->a[0]-b[0]);
+        int []dp = new int[pairs.length];
+        Arrays.fill(dp,1);
+        int ans = 1;
 
+        for(int i=0;i<pairs.length;i++){
+            for(int j=0;j<i;j++){
+                if(pairs[j][1]<pairs[i][0]){
+                    dp[i] = Math.max(dp[j]+1,dp[i]);
+                }
+            }
+            ans = Math.max(dp[i],ans);
+        }
+        return ans;
+    }
+}
